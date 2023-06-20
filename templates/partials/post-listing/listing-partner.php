2080a5d6cc99899bc716b2f00e72b3b193e16829
msreeves <reevesy87@hotmail.co.uk>
2023-06-20 12:23:19 +0100

    update with scss build
    needs fixing for live load build with node js and laravel mix

--- templates/partials/post-listing/listing-partner.php
+++
@@ -1,7 +0,0 @@
-          <div class="col-lg-4 mx-auto">
-              <div class="listing-panel">
-                <div class="partner-listing-image">
-           <a href="<?php echo get_field('link'); ?>" target="_blank"> <?php the_post_thumbnail(); ?></a>
-          </div>
-         </div>
-             </div>
--- templates/partials/post-listing/listing-publication.php
+++
@@ -1,5 +0,0 @@
-          <div class="carousel-tile">
-           <div class="listing-panel">
-           <a href="<?php echo get_field('publication_link'); ?>" target="_blank"> <?php the_post_thumbnail(); ?></a>
-          </div>
-         </div>
