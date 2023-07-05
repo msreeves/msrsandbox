c28fb64a7be04657c97d3aab8741b0c8bb7840c3
msreeves <reevesy87@hotmail.co.uk>
2023-07-05 09:34:27 +0100

    add section, move stories

--- index.php
+++ index.php
@@ -68,44 +68,10 @@ get_header();
       ?>
 
       <?php if ( $all_banners->have_posts() ) : ?>
+            <section>
 <?php get_template_part( 'inc/components/banner' ); ?>
       </section>
       <?php endif; ?> 
-<section class="posts-tabs">
-    <h1> My Stories </h1>
-	<div class="container">
-         <?php get_template_part( 'inc/components/searchbar' ); ?>
-	<ul class="nav nav-tabs">
-<?php 
-
-$categories = get_categories($cat_args);
-
-foreach($categories as $cat) : ?>
-	<li class="js-filter-item"><a data-category="<?= $cat->term_id; ?>" href="<?= get_category_link($cat->term_id); ?>"><button><?= $cat->name; ?></button></a></li>
-<?php endforeach; ?>
-</ul>
-<div class="js-filter">
-	<?php
-	$args = array(
-		'post_type' => 'post',
-		'posts_per_page' => 3
-		);
-
-		$query = new WP_Query($args);
-
-		if($query->have_posts()) :
-			echo '<div class="container">';
-			echo '<div class="row">';
-			while($query->have_posts()) : $query->the_post();
-				get_template_part( 'templates/partials/post-listing/listing-posts' );
-			endwhile;
-			echo '</div>';
-			echo '</div>';
-			endif;
-			wp_reset_postdata(); ?>
-</div>
-		</div>
-</section>
 <?php
 		$today = date('Ymd');
 		$args = [
@@ -299,6 +265,41 @@ if( $image ):
 }
 ?>
 </div>
+</section>
+<section class="posts-tabs">
+    <h1> My Stories </h1>
+	<div class="container">
+         <?php get_template_part( 'inc/components/searchbar' ); ?>
+	<ul class="nav nav-tabs">
+<?php 
+
+$categories = get_categories($cat_args);
+
+foreach($categories as $cat) : ?>
+	<li class="js-filter-item"><a data-category="<?= $cat->term_id; ?>" href="<?= get_category_link($cat->term_id); ?>"><button><?= $cat->name; ?></button></a></li>
+<?php endforeach; ?>
+</ul>
+<div class="js-filter">
+	<?php
+	$args = array(
+		'post_type' => 'post',
+		'posts_per_page' => -1
+		);
+
+		$query = new WP_Query($args);
+
+		if($query->have_posts()) :
+			echo '<div class="container">';
+			echo '<div class="row">';
+			while($query->have_posts()) : $query->the_post();
+				get_template_part( 'templates/partials/post-listing/listing-posts' );
+			endwhile;
+			echo '</div>';
+			echo '</div>';
+			endif;
+			wp_reset_postdata(); ?>
+</div>
+		</div>
 </section>
 
 </main>
