f2c1c517720e5c4dca5972e14e919928ab71b851
msreeves <reevesy87@hotmail.co.uk>
2023-12-10 09:13:00 +0000

    tag fix

--- templates/partials/post-listing/posts/maincategory.php
+++ templates/partials/post-listing/posts/maincategory.php
@@ -4,9 +4,18 @@
             	<?php the_post_thumbnail(); ?>
             </div>
             <div class="listing-text">
-              <p> <?php foreach((get_the_category()) as $category) {
-                echo '<a href="' . esc_url( get_category_link( $category ) ) . '"><span>' . $category->cat_name . '</span></a>';
-                } ?>
+                 <p>
+              <?php
+$exclude = array( 6 );
+$cat_list = array();
+foreach ( get_the_category() as $cat ) {
+    if ( ! in_array( $cat->term_id, $exclude ) ) {
+        $cat_list[] = '<a href="' . esc_url( get_category_link( $cat->term_id ) ) .
+            '"><span>' . $cat->name . '</span></a>';
+    }
+}
+
+echo implode( ', ', $cat_list ); ?>
                  </p>  
             <h3><?php the_title() ?></h3>                  
                      <p><?php echo wpse_custom_excerpts(30); ?></p>
